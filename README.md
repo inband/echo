a5b4613e35c22252cc2677c125a8af510eaa841b
inband <nat@inband.io>
2020-06-13 17:35:27 +1000

    echo server update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # echo
+
+**rfc 862** J.Postel 1983
+
+Python implementation TCP based echo server.  Sends back data it receives from source.
+
+* tcp port 7
+
+
--- echo_server.py
+++ echo_server.py
@@ -1,2 +1,47 @@
 import sys
 import socket
+
+
+host = ''
+port = 7
+
+address_family = socket.AF_INET
+transport_protocol = socket.SOCK_STREAM
+
+#create socket
+s = socket.socket(address_family, transport_protocol)
+
+#bind to socket
+try:
+    s.bind((host,port))
+except:
+    #OSError: [Errno 98] Address already in use
+    print('Address is already in use')
+
+#listen for connection
+s.listen(5)
+
+#accept connection
+conn, addr = s.accept()
+
+q = b'quit\r\n'
+while True:
+    data = conn.recv(1024)
+    if data == q:
+        s.shutdown(socket.SHUT_RDWR)
+        print('Client disconnected: {}'.format(addr))
+    else:
+        print(data.decode('utf-8'))
+        print(data.decode('ascii'))
+        print(data)
+        conn.send(data)
+                
+print(conn)
+print(addr)
+
+#s.shutdown(socket.SHUT_RDWR)
+#s.close()
+
+
+
+
