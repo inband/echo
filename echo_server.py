0874abef2d7a70e7c09d212fbee91acf337fa75b
inband <nat@inband.io>
2020-06-13 21:38:51 +1000

    threading

--- echo_server.py
+++ echo_server.py
@@ -1,6 +1,6 @@
 import sys
 import socket
-
+import threading
 
 host = ''
 port = 7
@@ -21,20 +21,32 @@ except:
 #listen for connection
 s.listen(5)
 
-#accept connection
-conn, addr = s.accept()
+def client_thread(conn, addr):
+    q = b'quit\r\n'
+    #conn, addr = accept
+    print('Client connected: {}'.format(addr))
+    while True:
+        data = conn.recv(1024)
+        if data == q:
+            conn.shutdown(socket.SHUT_RDWR)
+            print('Client disconnected: {}'.format(addr))      
+            conn.close()
+            return
+        else:
+            #print(data.decode('utf-8'))
+            #print(data.decode('ascii'))
+            print(data)
+            conn.send(data)
 
-q = b'quit\r\n'
+
 while True:
-    data = conn.recv(1024)
-    if data == q:
-        s.shutdown(socket.SHUT_RDWR)
-        print('Client disconnected: {}'.format(addr))
-    else:
-        print(data.decode('utf-8'))
-        print(data.decode('ascii'))
-        print(data)
-        conn.send(data)
+    threads = list()
+    conn, addr = s.accept()
+    #t = threading.Thread(target=client_thread(conn, addr))
+    t = threading.Thread(target=client_thread, args=(conn, addr,))
+    print(t)
+    threads.append(t)
+    t.start()
                 
 print(conn)
 print(addr)
